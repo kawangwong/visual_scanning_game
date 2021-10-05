bb75e8e190fa5477ec45472459526fa6bed59aac
kawangwong <nycazn724@gmail.com>
2021-10-05 16:59:48 -0400

    Add files via upload
    Decoupled functions and created user input for rounds

---
+++ functionfile.py
@@ -0,0 +1,8 @@
+from visualscanning import *
+
+rounds = int(input("how many rounds do you want this to go? "))
+
+i = 0
+while i != rounds:
+    start_game()
+    i += 1
--- visualscanning.py
+++ visualscanning.py
@@ -6,14 +6,17 @@ def block_printer(x):
     printed_letter = text2art(x, font='block', chr_ignore=True)
     return printed_letter
 
-'''First we need a random letter generator'''
-Letters = string.ascii_uppercase
-lucky_letter = random.choice(Letters)
-saved_letter = lucky_letter
-print(block_printer(lucky_letter))
-my_input = input("what letter just popped up? ").upper()
+'''Function used to create and show the block letters randomly here'''
 
-if saved_letter == my_input:
-    print("WOW YOU GOT IT RIGHT!")
-else:
-    print("WRONG!")
+def start_game():
+    '''First we need a random letter generator'''
+    Letters = string.ascii_uppercase
+    lucky_letter = random.choice(Letters)
+    saved_letter = lucky_letter
+    '''This will put the letter in a saved variable to be compared later with user response'''
+    print(block_printer(lucky_letter))
+    my_input = input("what letter just popped up? ").upper()
+    if saved_letter == my_input:
+        print("WOW YOU GOT IT RIGHT!")
+    else:
+        print("WRONG!")
