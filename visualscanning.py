7fd51defbd4c78ea6f95d2cdcee81435ab4153a2
kawangwong <nycazn724@gmail.com>
2021-10-06 15:33:56 -0400

    Add files via upload

--- visualscanning.py
+++ visualscanning.py
@@ -8,11 +8,9 @@ def block_printer(x):
     return printed_letter
 
 '''Function used to create and show the block letters randomly here'''
+correct = 0
 
-
-def start_game():
-    global correct
-    correct = 0
+def start_game(correct):
     '''First we need a random letter generator'''
     Letters = string.ascii_uppercase
     lucky_letter = random.choice(Letters)
@@ -22,11 +20,13 @@ def start_game():
         time.sleep(0.2)
         print("WOW YOU GOT IT RIGHT! You got 1 point!")
         correct += 1
+        return correct
     else:
         time.sleep(0.2)
         print(f"WRONG! This was the letter {lucky_letter}. Do better next time!")
+        return correct
 
-print(correct)
+print(start_game(correct))
 
 '''We need to also include code into this that would create a function that would save results
 this information could be detailed as in letters, or could be as simple as just rights and wrongs
