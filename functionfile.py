f98ca30e08f01e442e3f4f4d15f0571b4e3c4841
kawangwong <nycazn724@gmail.com>
2021-10-06 15:29:19 -0400

    Add files via upload

--- functionfile.py
+++ functionfile.py
@@ -1,8 +1,10 @@
 from visualscanning import *
 
-rounds = int(input("how many rounds do you want this to go? "))
+rounds = int(input("How many rounds do you want this to go for? "))
 
 i = 0
 while i != rounds:
     start_game()
     i += 1
+
+print(start_game(correct)/rounds)
