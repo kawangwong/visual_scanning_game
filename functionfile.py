6ea316f46aa84eca09b6cdc431f6b6e74f589ad9
kawangwong <nycazn724@gmail.com>
2021-10-06 16:04:34 -0400

    Add files via upload

--- functionfile.py
+++ functionfile.py
@@ -1,10 +1,17 @@
 from visualscanning import *
 
 rounds = int(input("How many rounds do you want this to go for? "))
-
+correct_answer = 0
+user_score = 0
 i = 0
 while i != rounds:
-    start_game()
+    correct_answer = start_game()
+    if correct_answer == 1:
+        user_score +=1
     i += 1
 
-print(start_game(correct)/rounds)
+print(user_score)
+
+percentage_correct = user_score*100/rounds
+
+print(f"Good job!, you got {percentage_correct}% correct out of {rounds} trials. ")
--- visualscanning.py
+++ visualscanning.py
@@ -8,9 +8,8 @@ def block_printer(x):
     return printed_letter
 
 '''Function used to create and show the block letters randomly here'''
-correct = 0
-
-def start_game(correct):
+def start_game():
+    x = 0
     '''First we need a random letter generator'''
     Letters = string.ascii_uppercase
     lucky_letter = random.choice(Letters)
@@ -19,15 +18,16 @@ def start_game(correct):
     if lucky_letter == my_input:
         time.sleep(0.2)
         print("WOW YOU GOT IT RIGHT! You got 1 point!")
-        correct += 1
-        return correct
+        x += 1
+        return x
     else:
         time.sleep(0.2)
         print(f"WRONG! This was the letter {lucky_letter}. Do better next time!")
-        return correct
+        return x
 
-print(start_game(correct))
-
+# start_game(x)
+# print(start_game(x))
+# print(x)
 '''We need to also include code into this that would create a function that would save results
 this information could be detailed as in letters, or could be as simple as just rights and wrongs
 More detail can be used to actually create datametrics for difficulty scales.'''
