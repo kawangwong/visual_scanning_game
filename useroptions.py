1d5b4fc3e9d65bc8298701e991cfdc784614e884
kawangwong <nycazn724@gmail.com>
2021-10-15 19:02:25 -0400

    Update useroptions.py

--- useroptions.py
+++ useroptions.py
@@ -1,7 +1,8 @@
 saveoption = True
-##Set this flag to true or false to allow result saving.
+##Set this flag to true or false to allow result saving. This is to save a default results.txt
 customletterflag = True
 ##Set flag here to True to use a custom letter list.
 custom_letters = ["a" ,"b" ,"c"]
 ##when flag is set to True, change the letters here, order and capitalization does not matter. It will always be uppercase.
 save_custom_list_option = True
+##set to save the data for custom letters. Will only be active when the custom letterflag is True.
